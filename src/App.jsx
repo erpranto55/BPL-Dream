7aa478d99fe8d3a0eb301be98acb07e0c7f4593f
erpranto <erpranto55@gmail.com>
2026-04-08 01:08:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './components/homepage/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/homepage/NavBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import dollarImage from "../../../assets/Currency.png"
+import { Menu } from 'lucide-react';
+
+const NavBar = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="navbar bg-base-100 shadow-sm ">
+            <div className='md:hidden relative'>
+                <button onClick={() => setOpen(!open)}>
+                    <Menu />
+                </button>
+
+                {
+                    open && (
+                        <ul className='absolute top-10 left-0 bg-white shadow-md p-4 flex flex-col gap-4 text-gray-500 w-40 z-50'>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Fixture</a></li>
+                            <li><a href="">Teams</a></li>
+                            <li><a href="">Schedules</a></li>
+                        </ul>
+                    )
+                }
+            </div>
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">BPL Dream 11</a>
+            </div>
+
+            <div className='flex gap-10'>
+                <ul className='text-gray-500 hidden md:flex md:gap-10' >
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Fixture</a></li>
+                    <li><a href="">Teams</a></li>
+                    <li><a href="">Schedules</a></li>
+                </ul>
+                <button className="flex-no flex justify-between items-center gap-2 font-bold text-xl">
+                    0 Coins
+                    <img src={dollarImage} alt="" />
+                </button>
+            </div>
+        </div >
+    );
+};
+
+export default NavBar;
