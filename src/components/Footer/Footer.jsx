ffd35288fffecda88396645693cc5be78506576f
erpranto <erpranto55@gmail.com>
2026-04-10 01:17:17 +0600

    Remove footer text from Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FooterBanner from './FooterBanner/FooterBanner'
+import FooterLogo from '../../assets/logo-footer.png'
 
 
 const Footer = () => {
@@ -8,8 +9,39 @@ const Footer = () => {
             <div className='absolute -top-50 left-1/2 transform -translate-x-1/2 w-full max-w-5xl'>
                 <FooterBanner />
             </div>
-            <div className='pt-32 text-white text-center'>
-                Footer
+
+            <div className='text-white text-center mx-auto pt-32 py-10 px-50'>
+                <img src={FooterLogo} className='mx-auto mb-10'></img>
+                <div className='grid md:grid-cols-3 gap-10 text-left'>
+                    <div classname=''>
+                        <h3 className='font-semibold mb-2'>About Us</h3>
+                        <p className='text-gray-400'>We are a passionate team <br />dedicated to providing the best<br /> services to our customers.</p>
+                    </div>
+                    <div>
+                        <h3 className='font-semibold mb-2'>Quick Links</h3>
+                        <ul className='text-gray-400 space-y-2'>
+                            <li><a>Home</a></li>
+                            <li><a>Services</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Contract</a></li>
+                        </ul>
+                    </div>
+
+                    <div >
+                        <h3 className='font-semibold mb-2'>Subscribe</h3>
+                        <p className='text-gray-400 mb-3'>Subscribe to our newsletter for the<br />latest updates.</p>
+                        <div className='flex mx-auto'>
+                            <input type="email" name="" id=""
+                                placeholder='Enter Your Email'
+                                className='px-3 py-2 w-full rounded-l-lg border border-gray-500 bg-transparent' />
+                            <button className='px-4 bg-linear-to-r from-blue-500 to-orange-500 rounded-r-lg'>
+                                Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+                <p className='text-gray-500 text-sm mt-10'>
+                    ©2024 Your Company All Rights Reserved.
+                </p>
             </div>
         </div>
 
--- src/components/Footer/FooterBanner/FooterBanner.jsx
+++ src/components/Footer/FooterBanner/FooterBanner.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 const FooterBanner = () => {
     return (
         <div className='max-w-5xl mx-auto p-6 rounded-3xl min-h-96'>
-            <div className='bg-linear-to-tr from-blue-200 to-orange-200 rounded-3xl flex flex-col justify-center text-center min-h-90 space-y-5'>
-                <h2 className='font-bold text-3xl'>Subscribe to our Newsletter</h2>
-                <h3 className='text-2xl text-gray-600'>Get the latest updates and news right in your inbox!</h3>
-                <div className='flex gap-5 mx-auto'>
+            <div className='bg-linear-to-tr from-blue-200 to-orange-200 rounded-3xl text-center min-h-90 space-y-5 shadow-lg'>
+                <h2 className='font-bold text-3xl pt-25'>
+                    Subscribe to our Newsletter
+                </h2>
+                <h3 className='text-xl text-gray-600'>
+                    Get the latest updates and news right in your inbox!
+                </h3>
+                <div className='flex gap-5 justify-center'>
                     <input type="email" name="" id=""
-                        placeholder='Enter Your Email'
+                        placeholder='  Enter Your Email'
                         className='border-2 border-gray-400 rounded-xl w-100' />
                     <button className='btn bg-linear-to-tr from-blue-500 to-orange-500 rounded-xl'>
                         Subscribe</button>
